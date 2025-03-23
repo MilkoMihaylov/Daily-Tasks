bad8533e19af93d63d78f6be8bc0f48e276d92f5
Milko <milko.m.mihaylov@gmail.com>
2025-03-23 18:17:54 +0200

    undo declined button now working

--- src/components/TaskContainer.tsx
+++ src/components/TaskContainer.tsx
@@ -1,5 +1,5 @@
 import { useDispatch } from 'react-redux';
-import { completeTask, declineTask } from '../store/taskSlice';
+import { completeTask, declineTask, undoTaskFromCompleted } from '../store/taskSlice';
 import './taskstyles.css'
 
 interface TaskContainerProps {
@@ -8,7 +8,7 @@ interface TaskContainerProps {
   taskStatus: 'tasks' | 'completed' | 'declined';
 }
 
-const TaskContainer: React.FC<TaskContainerProps> = ({ title, tasks, taskStatus }) => {
+function TaskContainer({ title, tasks, taskStatus }: TaskContainerProps) {
   const dispatch = useDispatch();
 
   const handleComplete = (id: number) => {
@@ -19,6 +19,11 @@ const TaskContainer: React.FC<TaskContainerProps> = ({ title, tasks, taskStatus 
     dispatch(declineTask(id));
   };
 
+  const handleUndoTaskFromCompleted = (id: number) => {
+    dispatch(undoTaskFromCompleted(id))
+  }
+  console.log(tasks);
+
   return (
     <div className='task-styles'>
       <h3>{title}</h3>
@@ -27,15 +32,29 @@ const TaskContainer: React.FC<TaskContainerProps> = ({ title, tasks, taskStatus 
           <li className='list-styles' key={task.id}>
             {task.name}
             {taskStatus === 'tasks' && (
-            <div>
-              <button className='approve' onClick={() => handleComplete(task.id)}>
-                ✅
-              </button>
-              <button className='decline' onClick={() => handleDecline(task.id)}>
-                ❌
-              </button>
-            </div>
-        )}
+              <div>
+                <button className='approve' onClick={() => handleComplete(task.id)}>
+                  ✅
+                </button>
+                <button className='decline' onClick={() => handleDecline(task.id)}>
+                  ❌
+                </button>
+              </div>
+            )}
+            {taskStatus === 'completed' && (
+              <div>
+                <button className='undo' onClick={() => handleUndoTaskFromCompleted(task.id,)}>
+                  ✅
+                </button>
+              </div>
+            )}
+             {taskStatus === 'declined' && (
+              <div>
+                <button className='undo' onClick={() => handleUndoTaskFromCompleted(task.id,)}>
+                  ✅
+                </button>
+              </div>
+            )}
           </li>
         ))}
       </ul>
